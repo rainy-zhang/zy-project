b3e5fc6c542b79f502f93ee41a126f8b34842e70
rainy-zhang <791136444@qq.com>
2023-03-07 17:18:24 +0800

    first commit

---
+++ zy-service/zy-space/src/main/java/org/rainy/project/SpaceApplication.java
@@ -0,0 +1,20 @@
+package org.rainy.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author zhangyu
+ */
+@SpringBootApplication
+public class SpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceApplication.class, args);
+    }
+    
+}
